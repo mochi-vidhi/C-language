80674fd42e304fe6f7b240c0c19e80be83233955
Mochi Vidhi <148563893+mochi-vidhi@users.noreply.github.com>
2024-02-07 16:07:41 +0000

    comments

---
+++ Comments/Multiline.c
@@ -0,0 +1,19 @@
+/* C program to illustrate 
+use of 
+multi-line comment */
+#include <stdio.h>
+int main()
+{
+    /*
+    This is a
+    multi-line comment
+    */
+   
+      /*
+    This comment contains some code which
+    will not be executed.
+    printf("Code enclosed in Comment");
+    */
+    printf("Welcome to Github.com");
+    return 0;
+}
---
+++ Comments/singleline.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    // This is a single-line comment
+    printf("Welcome to Github.com");
+    return 0;
+}
