14f068a83506081b18163bdf984cf0a3c04b7108
Mochi Vidhi <148563893+mochi-vidhi@users.noreply.github.com>
2024-02-08 10:02:07 +0000

    programs on operators

---
+++ Operators/ Relational.c
@@ -0,0 +1,13 @@
+// C program to illustrate the relational operators
+#include <stdio.h>
+int main(){
+	int a = 25, b = 5;
+	// using operators and printing results
+	printf("a < b : %d\n", a < b);
+	printf("a > b : %d\n", a > b);
+	printf("a <= b: %d\n", a <= b);
+	printf("a >= b: %d\n", a >= b);
+	printf("a == b: %d\n", a == b);
+	printf("a != b : %d\n", a != b);
+	return 0;
+}
---
+++ Operators/Logical.c
@@ -0,0 +1,10 @@
+// C program to illustrate the logical operators
+#include <stdio.h>
+int main(){
+	int a = 25, b = 5;
+	// using operators and printing results
+	printf("a && b : %d\n", a && b);
+	printf("a || b : %d\n", a || b);
+	printf("!a: %d\n", !a);
+	return 0;
+}
