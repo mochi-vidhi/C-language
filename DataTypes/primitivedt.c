5c63bc22a54eed5b81f896e26fdd3d29e1cab317
Mochi Vidhi <148563893+mochi-vidhi@users.noreply.github.com>
2024-02-08 08:47:25 +0000

    primitive_sizeof

---
+++ DataTypes/primitivedt.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+	int a = 9;
+	float b = 4.5;
+	char c = 'A';
+	double d = 123123123.00;
+	printf("Integer value with data: %d\n", a);
+	printf("Float value with  data: %f\n", b);
+	printf("Character value with data: %c\n",c);
+	printf("Double value with data : %lf", d);
+	return 0;
+}
